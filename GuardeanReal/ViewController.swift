bc53aa9395143d0af5d72b7216948a2e61dc4393
user <user@MacBook-Pro-user.local>
2017-11-05 14:08:42 +0900

    Initial Commit

---
+++ GuardeanReal/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GuardeanReal
+//
+//  Created by user on 05.11.17.
+//  Copyright © 2017 user. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
