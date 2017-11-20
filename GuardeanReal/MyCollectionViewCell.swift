802083150255e8f41bda54ad056cefb7335b4d6d
user <user@MacBook-Pro-user.local>
2017-11-20 21:31:35 +0900

    first commit

---
+++ GuardeanReal/MyCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  MyCollectionViewCell.swift
+//  GuardeanReal
+//
+//  Created by user on 12.11.17.
+//  Copyright © 2017 user. All rights reserved.
+//
+
+import UIKit
+
+class MyCollectionViewCell: UICollectionViewCell {
+    
+}
